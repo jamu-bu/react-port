d45bf827d443be170c1e67bc594d441f423370da
jamu-bu <jamu@bu.edu>
2023-11-10 20:11:46 -0800

    added porftolio page and fixed about page

--- main/src/components/nav.jsx
+++ main/src/components/nav.jsx
@@ -1,17 +1,29 @@
 // Bringing in the required import from 'react-router-dom'
 //import { Link } from 'react-router-dom'
 
+import AboutPage from "../pages/AboutPage";
 import Eyelogo from "./UI/eyeopen";
 
 export default function Nav() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
+      
     <div>
-        <Eyelogo/>
-        <div><a href="/about"><h1>ABOUT</h1></a></div>
-        <div><a href="/portfolio"><h1>PORTFOLIO</h1></a></div>
-        <div><a href="/resume"><h1>RESUME</h1></a></div>
-        <div><a href="/contact"><h1>CONTACT</h1></a></div>
+        <div>
+          <h1 id="name">
+          차<br/>
+          미<br/>
+          경<br/>
+          </h1>
+        </div>
+        <div><Eyelogo/></div>
+        <AboutPage/>
+        <div id="nav-container">
+          
+          <div><a href="#portfolio"><h1 id="port">PORTFOLIO</h1></a></div>
+          <div><a href="#resume"><h1 id="resume">RESUME</h1></a></div>
+          <div><a href="#contact"><h1 id="contact">CONTACT</h1></a></div>
+        </div>
       
     </div>
   );
--- main/src/pages/AboutPage.jsx
+++ main/src/pages/AboutPage.jsx
@@ -1,65 +1,20 @@
 // This is a static page mocking an "About Us" section for our fake user data
+
+import { useState } from "react";
+
 export default function AboutPage() {
+    const [section, setSection]=useState(false)
+    console.log(section);
     return (
-      <div className="container pt-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-          nostrum temporibus ad omnis nam rerum eligendi.
-        </p>
-        <section className="features-icons bg-light text-center m-4">
-          <div className="container">
-            <div className="row p-2">
-              <div className="col-lg-4">
-                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                  <div className="features-icons-icon d-flex">
-                    <i className="bi-window m-auto text-primary" />
-                  </div>
-                  <h3>Fully Responsive</h3>
-                  <p className="lead mb-0">
-                    This theme will look great on any device, no matter the size!
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                  <div className="features-icons-icon d-flex">
-                    <i className="bi-layers m-auto text-primary" />
-                  </div>
-                  <h3>Bootstrap 5 Ready</h3>
-                  <p className="lead mb-0">
-                    Featuring the latest build of the new Bootstrap 5 framework!
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                  <div className="features-icons-icon d-flex">
-                    <i className="bi-terminal m-auto text-primary" />
-                  </div>
-                  <h3>Easy to Use</h3>
-                  <p className="lead mb-0">
-                    Ready to use with your own content, or customize the source
-                    files!
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-          nostrum temporibus ad omnis nam rerum eligendi.
-        </p>
+        
+      <div>
+        <div><a href="#" onClick={() => setSection(!section)}><h1 id="about">ABOUT</h1></a></div>
+        { section ?
+            <p className="textion">
+                Jamie is interested in UX UI design. She would like to further pursue the Arts in a digital medium whether it be through design, or the implemenation of 
+                front end elements. In her free time she enjoys going to Art Museums and creating 3D models in Blender. 
+            </p> : null
+        }
       </div>
     );
   }
